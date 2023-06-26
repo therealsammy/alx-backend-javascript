f561a5fb5f681ba280f09f693e1681a908bfbc41
Samuel Ohiri <therealsammy18x@gmail.com>
2023-06-26 12:57:42 +0100

    first commit

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const result = [];
+
+    for (let value of array) {
+        result.push(appendString + value);
+    }
+
+    return result;
+}
