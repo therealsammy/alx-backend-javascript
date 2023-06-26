1e0ce17ac339153a56fab6a63727393d6c40b4d8
Samuel Ohiri <therealsammy18x@gmail.com>
2023-06-26 13:09:06 +0100

    ESLint revoke

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,18 +1,15 @@
-// Function to return a string task
 export function taskFirst() {
-    const task = 'I prefer const when I can.'; // Declare and initialize a constant variable 'task' with a string value
-    return task; // Return the 'task' variable
+    const task = 'I prefer const when I can.';
+    return task;
 }
 
-// Function to return a string indicating something is okay
 export function getLast() {
-    return ' is okay'; // Return the string ' is okay'
+    return ' is okay';
 }
 
-// Function to concatenate a string with the result of calling the 'getLast' function
 export function taskNext() {
-    let combination = 'But sometimes let'; // Declare and initialize a variable 'combination' with a string value
-    combination += getLast(); // Append the result of calling the 'getLast' function to the 'combination' variable
+    let combination = 'But sometimes let';
+    combination += getLast();
 
-    return combination; // Return the 'combination' variable
+    return combination;
 }
